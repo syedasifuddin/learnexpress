85798da87d6bd02aafa86d14374954ec98cba635
Syed Asif <aftabsyedasif@gmail.com>
2024-02-06 17:07:40 +0530

    Exploring route handlers

--- index.js
+++ index.js
@@ -7,9 +7,40 @@ app.all('/secret', (req, res, next) => {
     next() // pass control to the next handler
 })
 
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendStatus(200);
+})
+
+app.get('/example/a', (req, res) => {
+    res.send('Hello from A!')
+})
+
+app.get('/example/b', (req, res, next) => {
+    console.log('the response will be sent by the next function ...')
+    next()
+}, (req, res) => {
+    res.send('Hello from B!')
+})
+
+const cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+}
+
+const cb1 = function (req, res, next) {
+    console.log('CB1')
+    next()
+}
+
+const cb2 = function (req, res) {
+    res.send('Hello from C!')
+}
+
+app.get('/example/c', cb0, cb1, cb2)
+
+
+app.get('/users/:userId-:bookId', (req, res) => {
+    res.send(req.params)
 })
 
 app.post('/', (req, res) => {
